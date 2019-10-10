09a080404015d6e5be3631f725716f92ca984759
alessandro-pianetta <alessandro.pianetta@gmail.com>
2019-10-10 16:13:05 -0700

    Fixes to landing styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,11 +7,9 @@ import Layout from "../components/layout"
 export default () => (
   <Layout>
     <div className={indexStyles.primaryContainer}>
-      <div>
-        <h1 className={indexStyles.header}>
-          Track the money in San José elections
-        </h1>
-      </div>
+      <h1 className={indexStyles.header}>
+        Track the money in San José elections
+      </h1>
       <ul className={indexStyles.headerList}>
         <li className={indexStyles.headerListItem}>
           Who is contributing to the candidates?
@@ -26,18 +24,18 @@ export default () => (
           Are the sources of political spending local?
         </li>
       </ul>
-      <div>
-        <h2 className={indexStyles.subheader}>
-          Total contributions flowing into San José’s 2018 Election:
-        </h2>
-      </div>
-      <p className={indexStyles.amount}>$6,297,678</p>
-      <button className={indexStyles.headerButton}>
-        <Link className={indexStyles.headerButtonText} to="/page-2/">
-          Follow the money
+      <h2 className={indexStyles.subheader}>
+        Total contributions flowing into San José’s 2018 Election:
+      </h2>
+      <p className={indexStyles.totalFundsRaised}>$6,297,678</p>
+      <button className={indexStyles.button}>
+        <Link className={indexStyles.buttonText} to="/page-2/">
+          <strong className={indexStyles.headerButtonText}>
+            Follow the money
+          </strong>
         </Link>
       </button>
-      <div className={indexStyles.credit}>
+      <div className={indexStyles.imgCredit}>
         <p>
           Modified from source by{" "}
           <a href="https://unsplash.com/@odya_kun?utm_medium=referral&amp;utm_campaign=photographer-credit&amp;utm_content=creditBadge">
@@ -46,48 +44,39 @@ export default () => (
         </p>
       </div>
     </div>
-    <div className={indexStyles.subContainer}>
-      <div>
-        <h3 className={indexStyles.topText}>
-          Open Disclosure was created to empower San José voters with timely,
-          accurate, and useful information about the role of money in local
-          elections.
-        </h3>
-      </div>
-      <div>
-        <p className={indexStyles.subText}>
-          This site pulls data from the City of San José’s campaign finance
-          reporting database to produce easy to understand graphs that clearly
-          show the source of campaign funds and how they are being spent to sway
-          election results. Our goal is to increase government transparency by
-          revealing the potential influence behind local politics.
-        </p>
-      </div>
-      <div>
-        <p className={indexStyles.subText}>
-          We hope to raise awareness, promote electoral accountability, and
-          promote civic engagement and political participation by Oakland
-          residents.
-        </p>
-      </div>
+    <div className={indexStyles.landingDescription}>
+      <h3 className={indexStyles.landingDescriptionHeader}>
+        Open Disclosure was created to empower San José voters with timely,
+        accurate, and useful information about the role of money in local
+        elections.
+      </h3>
+      <p className={indexStyles.landingDescriptionParagraph}>
+        This site pulls data from the City of San José’s campaign finance
+        reporting database to produce easy to understand graphs that clearly
+        show the source of campaign funds and how they are being spent to sway
+        election results. Our goal is to increase government transparency by
+        revealing the potential influence behind local politics.
+      </p>
+      <p className={indexStyles.landingDescriptionParagraph}>
+        We hope to raise awareness, promote electoral accountability, and
+        promote civic engagement and political participation by San José
+        residents.
+      </p>
     </div>
+
     <div className={indexStyles.border} />
 
-    <div className={indexStyles.subContainer}>
-      <div>
-        <h3 className={indexStyles.topText}>
-          Sign up for daily campaign finance alerts.
-        </h3>
-      </div>
-      <div>
-        <p className={indexStyles.subText}>
-          Every day, San José's lobbyists, candidates, and decisionmakers are
-          required to disclose their finances. Open Disclosure Alerts help you
-          stay on top of the influence of money on your local politics.
-        </p>
-      </div>
-      <button className={indexStyles.headerButton}>
-        <Link className={indexStyles.headerButtonText} to="/page-2/">
+    <div className={indexStyles.landingDescription}>
+      <h3 className={indexStyles.landingDescriptionHeader}>
+        Sign up for daily campaign finance alerts.
+      </h3>
+      <p className={indexStyles.landingDescriptionParagraph}>
+        Every day, San José's lobbyists, candidates, and decisionmakers are
+        required to disclose their finances. Open Disclosure Alerts help you
+        stay on top of the influence of money on your local politics.
+      </p>
+      <button className={indexStyles.button}>
+        <Link className={indexStyles.buttonText} to="/page-2/">
           Sign up for daily alerts
         </Link>
       </button>
