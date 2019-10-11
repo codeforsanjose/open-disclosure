7e0d9ff642ec111e0d0c3dfc7f3fbad3fd26cc0b
alessandro-pianetta <alessandro.pianetta@gmail.com>
2019-10-10 17:02:47 -0700

    Converts components from functional to class based

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,87 +3,94 @@ import { Link } from "gatsby"
 import layoutStyles from "./layout.module.css"
 import "typeface-roboto"
 
-const links = [
-  {
-    name: "About",
-    endpoint: "",
-  },
-  {
-    name: "Press",
-    endpoint: "",
-  },
-  {
-    name: "FAQ",
-    endpoint: "",
-  },
-  {
-    name: "Join us",
-    endpoint: "",
-  },
-  {
-    name: "Open Source",
-    endpoint: "",
-  },
-  {
-    name: "Register to vote",
-    endpoint: "",
-  },
-]
+export default class Layout extends React.PureComponent {
+  links = [
+    {
+      name: "About",
+      endpoint: "",
+    },
+    {
+      name: "Press",
+      endpoint: "",
+    },
+    {
+      name: "FAQ",
+      endpoint: "",
+    },
+    {
+      name: "Join us",
+      endpoint: "",
+    },
+    {
+      name: "Open Source",
+      endpoint: "",
+    },
+    {
+      name: "Register to vote",
+      endpoint: "",
+    },
+  ]
 
-export default props => (
-  <div className={layoutStyles.container}>
-    {props.children}
-    <div className={layoutStyles.footer}>
-      <div className={layoutStyles.footerLeft}>
-        <div className={layoutStyles.header}>
-          <h2 className={layoutStyles.headerText}>
-            Open<span className={layoutStyles.headerTextBlue}>Disclosure</span>{" "}
-            <span className={layoutStyles.headerTextBold}>San José</span>
-          </h2>
-          <nav>
-            <ul className={layoutStyles.footerNav}>
-              {links.map(item => (
-                <li className={layoutStyles.footerNavItem}>
-                  <Link
-                    className={`${layoutStyles.footerNavItemText} ${layoutStyles.link}`}
-                    to={item.endpoint}
-                  >
-                    {item.name}
-                  </Link>
-                </li>
-              ))}
-            </ul>
-          </nav>
-        </div>
-        <div className={layoutStyles.footerBottomContent}>
-          <h5 className={layoutStyles.tagline}>
-            Brought to you by
-            <a
-              className={layoutStyles.link}
-              href="https://www.codeforsanjose.com/"
-            >
-              {" "}
-              Code for San José
-            </a>
-          </h5>
-          <h6 className={layoutStyles.footerStipulation}>
-            Campaign finance data provided by the City of San José Public Ethics
-            Commission Public Portal for Campaign Finance and Lobbyist
-            Disclosure. Candidate and ballot measure information gathered from
-            information provided to the Santa Clara County Registrar of Voters
-            by the City of San José.
-          </h6>
+  render() {
+    return (
+      <div className={layoutStyles.container}>
+        {this.props.children}
+        <div className={layoutStyles.footer}>
+          <div className={layoutStyles.footerLeft}>
+            <div className={layoutStyles.header}>
+              <h2 className={layoutStyles.headerText}>
+                Open
+                <span className={layoutStyles.headerTextBlue}>
+                  Disclosure
+                </span>{" "}
+                <span className={layoutStyles.headerTextBold}>San José</span>
+              </h2>
+              <nav>
+                <ul className={layoutStyles.footerNav}>
+                  {this.links.map(item => (
+                    <li className={layoutStyles.footerNavItem}>
+                      <Link
+                        className={`${layoutStyles.footerNavItemText} ${layoutStyles.link}`}
+                        to={item.endpoint}
+                      >
+                        {item.name}
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
+              </nav>
+            </div>
+            <div className={layoutStyles.footerBottomContent}>
+              <h5 className={layoutStyles.tagline}>
+                Brought to you by
+                <a
+                  className={layoutStyles.link}
+                  href="https://www.codeforsanjose.com/"
+                >
+                  {" "}
+                  Code for San José
+                </a>
+              </h5>
+              <h6 className={layoutStyles.footerStipulation}>
+                Campaign finance data provided by the City of San José Public
+                Ethics Commission Public Portal for Campaign Finance and
+                Lobbyist Disclosure. Candidate and ballot measure information
+                gathered from information provided to the Santa Clara County
+                Registrar of Voters by the City of San José.
+              </h6>
+            </div>
+          </div>
+          <div className={layoutStyles.footerRight}>
+            <iframe
+              title="Enter your address for personalized voting information"
+              src="https://votersedge.org/en/ca/address-widget"
+              frameBorder="0"
+              scrolling="no"
+              style={{ width: "240px", height: "200px" }}
+            ></iframe>
+          </div>
         </div>
       </div>
-      <div className={layoutStyles.footerRight}>
-        <iframe
-          title="Enter your address for personalized voting information"
-          src="https://votersedge.org/en/ca/address-widget"
-          frameBorder="0"
-          scrolling="no"
-          style={{ width: "240px", height: "200px" }}
-        ></iframe>
-      </div>
-    </div>
-  </div>
-)
+    )
+  }
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,88 +4,96 @@ import { Link } from "gatsby"
 import "typeface-roboto"
 import Layout from "../components/layout"
 
-const splashImage = {
-  photographer: "Andrii Ganzevych",
-  url:
-    "https://unsplash.com/@odya_kun?utm_medium=referral&amp;utm_campaign=photographer-credit&amp;utm_content=creditBadge",
+export default class MainPage extends React.PureComponent {
+  splashImage = {
+    photographer: "Andrii Ganzevych",
+    url:
+      "https://unsplash.com/@odya_kun?utm_medium=referral&amp;utm_campaign=photographer-credit&amp;utm_content=creditBadge",
+  }
+  totalFundsRaised = "$6,297,678"
+  electionYear = "2018"
+
+  render() {
+    return (
+      <Layout>
+        <div className={indexStyles.primaryContainer}>
+          <h1 className={indexStyles.header}>
+            Track the money in San José elections
+          </h1>
+          <ul className={indexStyles.headerList}>
+            <li className={indexStyles.headerListItem}>
+              Who is contributing to the candidates?
+            </li>
+            <li className={indexStyles.headerListItem}>
+              How much money has each candidate raised?
+            </li>
+            <li className={indexStyles.headerListItem}>
+              Who is spending money to influence local ballot measures outcomes?
+            </li>
+            <li className={indexStyles.headerListItem}>
+              Are the sources of political spending local?
+            </li>
+          </ul>
+          <h2 className={indexStyles.subheader}>
+            Total contributions flowing into San José’s {this.electionYear}{" "}
+            Election:
+          </h2>
+          <p className={indexStyles.totalFundsRaised}>
+            {this.totalFundsRaised}
+          </p>
+          <button className={indexStyles.button}>
+            <Link className={indexStyles.buttonText} to="/page-2/">
+              <strong className={indexStyles.headerButtonText}>
+                Follow the money
+              </strong>
+            </Link>
+          </button>
+          <div className={indexStyles.imgCredit}>
+            <p>
+              Modified from source by{" "}
+              <a href={this.splashImage.url}>{this.splashImage.photographer}</a>
+            </p>
+          </div>
+        </div>
+        <div className={indexStyles.landingDescription}>
+          <h3 className={indexStyles.landingDescriptionHeader}>
+            Open Disclosure was created to empower San José voters with timely,
+            accurate, and useful information about the role of money in local
+            elections.
+          </h3>
+          <p className={indexStyles.landingDescriptionParagraph}>
+            This site pulls data from the City of San José’s campaign finance
+            reporting database to produce easy to understand graphs that clearly
+            show the source of campaign funds and how they are being spent to
+            sway election results. Our goal is to increase government
+            transparency by revealing the potential influence behind local
+            politics.
+          </p>
+          <p className={indexStyles.landingDescriptionParagraph}>
+            We hope to raise awareness, promote electoral accountability, and
+            promote civic engagement and political participation by San José
+            residents.
+          </p>
+        </div>
+
+        <div className={indexStyles.border} />
+
+        <div className={indexStyles.landingDescription}>
+          <h3 className={indexStyles.landingDescriptionHeader}>
+            Sign up for daily campaign finance alerts.
+          </h3>
+          <p className={indexStyles.landingDescriptionParagraph}>
+            Every day, San José's lobbyists, candidates, and decisionmakers are
+            required to disclose their finances. Open Disclosure Alerts help you
+            stay on top of the influence of money on your local politics.
+          </p>
+          <button className={indexStyles.button}>
+            <Link className={indexStyles.buttonText} to="/page-2/">
+              Sign up for daily alerts
+            </Link>
+          </button>
+        </div>
+      </Layout>
+    )
+  }
 }
-const totalFundsRaised = "$6,297,678"
-const electionYear = "2018"
-
-export default () => (
-  <Layout>
-    <div className={indexStyles.primaryContainer}>
-      <h1 className={indexStyles.header}>
-        Track the money in San José elections
-      </h1>
-      <ul className={indexStyles.headerList}>
-        <li className={indexStyles.headerListItem}>
-          Who is contributing to the candidates?
-        </li>
-        <li className={indexStyles.headerListItem}>
-          How much money has each candidate raised?
-        </li>
-        <li className={indexStyles.headerListItem}>
-          Who is spending money to influence local ballot measures outcomes?
-        </li>
-        <li className={indexStyles.headerListItem}>
-          Are the sources of political spending local?
-        </li>
-      </ul>
-      <h2 className={indexStyles.subheader}>
-        Total contributions flowing into San José’s {electionYear} Election:
-      </h2>
-      <p className={indexStyles.totalFundsRaised}>{totalFundsRaised}</p>
-      <button className={indexStyles.button}>
-        <Link className={indexStyles.buttonText} to="/page-2/">
-          <strong className={indexStyles.headerButtonText}>
-            Follow the money
-          </strong>
-        </Link>
-      </button>
-      <div className={indexStyles.imgCredit}>
-        <p>
-          Modified from source by{" "}
-          <a href={splashImage.url}>{splashImage.photographer}</a>
-        </p>
-      </div>
-    </div>
-    <div className={indexStyles.landingDescription}>
-      <h3 className={indexStyles.landingDescriptionHeader}>
-        Open Disclosure was created to empower San José voters with timely,
-        accurate, and useful information about the role of money in local
-        elections.
-      </h3>
-      <p className={indexStyles.landingDescriptionParagraph}>
-        This site pulls data from the City of San José’s campaign finance
-        reporting database to produce easy to understand graphs that clearly
-        show the source of campaign funds and how they are being spent to sway
-        election results. Our goal is to increase government transparency by
-        revealing the potential influence behind local politics.
-      </p>
-      <p className={indexStyles.landingDescriptionParagraph}>
-        We hope to raise awareness, promote electoral accountability, and
-        promote civic engagement and political participation by San José
-        residents.
-      </p>
-    </div>
-
-    <div className={indexStyles.border} />
-
-    <div className={indexStyles.landingDescription}>
-      <h3 className={indexStyles.landingDescriptionHeader}>
-        Sign up for daily campaign finance alerts.
-      </h3>
-      <p className={indexStyles.landingDescriptionParagraph}>
-        Every day, San José's lobbyists, candidates, and decisionmakers are
-        required to disclose their finances. Open Disclosure Alerts help you
-        stay on top of the influence of money on your local politics.
-      </p>
-      <button className={indexStyles.button}>
-        <Link className={indexStyles.buttonText} to="/page-2/">
-          Sign up for daily alerts
-        </Link>
-      </button>
-    </div>
-  </Layout>
-)
