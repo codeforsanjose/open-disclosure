3602518981933401a18066c24f94323b096f3aa4
alessandro-pianetta <alessandro.pianetta@gmail.com>
2020-04-16 15:01:15 -0700

    Reworked layout component for new design and SCSS

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,24 +1,40 @@
 import React from "react"
 import { Link } from "gatsby"
-import layoutStyles from "./layout.module.css"
+import layoutStyles from "./layout.module.scss"
 import "typeface-roboto"
+import "typeface-bitter"
 
 export default class Layout extends React.PureComponent {
+  state = {
+    value: "",
+  }
+
   links = [
     {
-      name: "About",
+      name: "Ballot Measures",
       endpoint: "/",
     },
     {
-      name: "Press",
+      name: "Candidates",
+      endpoint: "/",
+    },
+    {
+      name: "About",
       endpoint: "/",
     },
     {
       name: "FAQ",
       endpoint: "/",
     },
+  ]
+
+  footerLinks = [
     {
-      name: "Join us",
+      name: "Join Us",
+      endpoint: "/",
+    },
+    {
+      name: "Find Your Ballot",
       endpoint: "/",
     },
     {
@@ -26,99 +42,128 @@ export default class Layout extends React.PureComponent {
       endpoint: "/",
     },
     {
-      name: "Register to vote",
+      name: "Press",
       endpoint: "/",
     },
   ]
 
-  renderHeader() {
-    const { hideHeader, windowIsLarge } = this.props
+  handleSubmit() {}
 
-    if (hideHeader) return null
+  handleChange = ({ target: { value } }) => {
+    console.log(value)
+    this.setState({ value })
+  }
+
+  renderLogo(truncate = true) {
+    const { windowIsLarge } = this.props
+    const logo = () => (
+      <p>
+        <strong>Open Disclosure</strong> San José
+      </p>
+    )
+
     return (
-      <div className={layoutStyles.topBar}>
-        <Link className={layoutStyles.topBarText} to={"/"}>
-          {windowIsLarge ? "Open Disclosure San José" : "ODSJ"}
-        </Link>
-        <nav>
-          <ul className={layoutStyles.headerNav}>
-            {this.links.slice(0, 3).map((item, index) => (
-              <li
-                key={`header nav item ${index}`}
-                className={layoutStyles.headerNavItem}
-              >
-                <Link
-                  className={`${layoutStyles.headerNavItemText} ${layoutStyles.link}`}
-                  to={item.endpoint}
-                >
-                  {item.name}
-                </Link>
-              </li>
-            ))}
-          </ul>
-        </nav>
-      </div>
+      <Link className={layoutStyles.headerText} to={"/"}>
+        {windowIsLarge ? logo() : truncate ? <p>ODSJ</p> : logo()}
+      </Link>
     )
   }
 
   render() {
     return (
       <div className={layoutStyles.container}>
-        {this.renderHeader()}
+        {this.props.hideHeader ? null : (
+          <header className={layoutStyles.header}>
+            {this.renderLogo()}
+            <nav>
+              <ul className={layoutStyles.headerNav}>
+                {this.links.map((item, index) => (
+                  <li
+                    key={`header nav item ${index}`}
+                    className={layoutStyles.headerNavItem}
+                  >
+                    <Link className={layoutStyles.link} to={item.endpoint}>
+                      <strong className={layoutStyles.headerNavItemText}>
+                        {item.name}
+                      </strong>
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </nav>
+          </header>
+        )}
         {this.props.children}
         <div className={layoutStyles.footer}>
-          <div className={layoutStyles.footerLeft}>
-            <div className={layoutStyles.header}>
-              <h2 className={layoutStyles.headerText}>
-                Open
-                <span className={layoutStyles.headerTextBlue}>Disclosure</span>
-                <span className={layoutStyles.headerTextBold}> San José</span>
-              </h2>
+          <div className={layoutStyles.footerTop}>
+            <div className={layoutStyles.footerLeft}>
+              {this.renderLogo(false)}
               <nav>
                 <ul className={layoutStyles.footerNav}>
-                  {this.links.map((item, index) => (
-                    <li
-                      key={`footer nav item ${index}`}
-                      className={layoutStyles.footerNavItem}
-                    >
-                      <Link
-                        className={`${layoutStyles.footerNavItemText} ${layoutStyles.link}`}
-                        to={item.endpoint}
+                  <div className={layoutStyles.footerNavSection}>
+                    {this.links.map((item, index) => (
+                      <li
+                        key={`footer nav item ${index}`}
+                        className={layoutStyles.footerNavItem}
                       >
-                        {item.name}
-                      </Link>
-                    </li>
-                  ))}
+                        <Link
+                          className={`${layoutStyles.footerNavItemText} ${layoutStyles.link}`}
+                          to={item.endpoint}
+                        >
+                          {item.name}
+                        </Link>
+                      </li>
+                    ))}
+                  </div>
+                  <div className={layoutStyles.footerNavSection}>
+                    {this.footerLinks.map((item, index) => (
+                      <li
+                        key={`footer nav item ${index}`}
+                        className={layoutStyles.footerNavItem}
+                      >
+                        <Link
+                          className={`${layoutStyles.footerNavItemText} ${layoutStyles.link}`}
+                          to={item.endpoint}
+                        >
+                          {item.name}
+                        </Link>
+                      </li>
+                    ))}
+                  </div>
                 </ul>
               </nav>
             </div>
-            <div className={layoutStyles.footerBottomContent}>
-              <h5 className={layoutStyles.tagline}>
-                Brought to you by
-                <a
-                  className={layoutStyles.link}
-                  href="https://www.codeforsanjose.com/"
-                >
-                  Code for San José
-                </a>
-              </h5>
-              <h6 className={layoutStyles.footerStipulation}>
-                Campaign finance data provided by the City of San José Public
-                Ethics Commission Public Portal for Campaign Finance and
-                Lobbyist Disclosure. Candidate and ballot measure information
-                gathered from information provided to the Santa Clara County
-                Registrar of Voters by the City of San José.
-              </h6>
+            <div className={layoutStyles.footerRight}>
+              <div className={layoutStyles.footerRightInner}>
+                {/* To-Do: Replace with image */}
+                <div className={layoutStyles.imgPlaceholder} />
+                <h5>Updates delivered to your inbox</h5>
+                <form onSubmit={this.handleSubmit}>
+                  <input
+                    className={layoutStyles.emailInput}
+                    type="email"
+                    placeholder="Enter your email address"
+                    value={this.state.value}
+                    onChange={this.handleChange}
+                  />
+                  <input
+                    className={layoutStyles.submitForm}
+                    type="submit"
+                    value="Subscribe"
+                  />
+                </form>
+              </div>
             </div>
           </div>
-          <div className={layoutStyles.footerRight}>
-            <iframe
-              title="Enter your address for personalized voting information"
-              src="https://votersedge.org/en/ca/address-widget"
-              frameBorder="0"
-              scrolling="no"
-              style={{ width: "240px", height: "200px" }}
-            ></iframe>
+          <div className={layoutStyles.footerBottom}>
+            <p>Brought to you by Code for San José</p>
+            <p>
+              Campaign finance data provided by the City of San José Public
+              Ethics Commission Public Portal for Campaign Finance and Lobbyist
+              Disclosure. Candidate and ballot measure information gathered from
+              information provided to the Santa Clara County Registrar of Voters
+              by the City of San José.
+            </p>
           </div>
         </div>
       </div>
