dc2c7575149239eb5488bf9c97d81ef70e015e5d
Emily Janzer <emily.janzer@gmail.com>
2020-07-29 11:41:25 -0700

    Add header blob image

--- src/pages/registerToVote.js
+++ src/pages/registerToVote.js
@@ -2,6 +2,7 @@ import React from "react"
 import Layout from "../components/layout"
 import styles from "./registerToVote.module.scss"
 import { useEffect, useState } from "react"
+import orangeHeaderBlob from "./../../static/images/orangeHeaderBlob.png"
 
 export default function RegisterToVote() {
   const [windowIsLarge, setWindowIsLarge] = useState(true)
@@ -21,6 +22,14 @@ export default function RegisterToVote() {
           <div className={styles.heroLeft}>
             <h1>Register to Vote</h1>
             <h2>Register to vote or see if you're already registered.</h2>
+          </div>
+          <div className={styles.heroRight}>
+            <img
+              alt="header"
+              className="responsive"
+              width="708px"
+              src={orangeHeaderBlob}
+            />
           </div>
         </header>
       </div>
