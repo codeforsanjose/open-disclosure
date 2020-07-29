674b7a0a24107047c1819e9deb1fde009cec2dac
Emily Janzer <emily.janzer@gmail.com>
2020-07-29 15:00:06 -0700

    Componentize

--- src/pages/registerToVote.js
+++ src/pages/registerToVote.js
@@ -2,8 +2,9 @@ import React from "react"
 import Layout from "../components/layout"
 import styles from "./registerToVote.module.scss"
 import { useEffect, useState } from "react"
-import orangeHeaderBlob from "./../../static/images/orangeHeaderBlob.png"
 import Button from "../common/button/index"
+import LandingPageHero from "../components/landingPageHero"
+import RegisterToVoteCard from "../components/registerToVoteCard"
 
 export default function RegisterToVote() {
   const [windowIsLarge, setWindowIsLarge] = useState(true)
@@ -20,36 +21,25 @@ export default function RegisterToVote() {
     <Layout windowIsLarge={windowIsLarge}>
       <div className={styles.container}>
         <header className={styles.hero}>
-          <div className={styles.heroLeft}>
-            <h1>Register to Vote</h1>
-            <h2>Register to vote or see if you're already registered.</h2>
-          </div>
-          <div className={styles.heroRight}>
-            <img
-              alt="header"
-              className="responsive"
-              width="708px"
-              src={orangeHeaderBlob}
-            />
-          </div>
+          <LandingPageHero
+            title="Register to Vote"
+            subtitle="Register to vote or see if you're already registered."
+          />
         </header>
         <div className={styles.body}>
-          <div className={`${styles.card} ${styles.green}`}>
-            <h2>Am I registered to vote?</h2>
-            <p>
-              Not sure if you're registered to vote? It takes 30 seconds to
-              confirm!
-            </p>
-            <Button text="Check now" />
-          </div>
-          <div className={`${styles.card} ${styles.blue}`}>
-            <h2>Register to vote!</h2>
-            <p>
-              Ready to make your voice heard in this upcoming election? Register
-              to vote in less than two minutes.
-            </p>
-            <Button text="Register to vote" />
-          </div>
+          <RegisterToVoteCard
+            color="green"
+            title="Am I registered to vote?"
+            body="Not sure if you're registered to vote? It takes 30 seconds to confirm!"
+            cta="Check now"
+          />
+          <RegisterToVoteCard
+            color="blue"
+            title="Register to vote!"
+            body="Ready to make your voice heard in this upcoming election? Register
+              to vote in less than two minutes."
+            cta="Register to vote"
+          />
         </div>
       </div>
     </Layout>
