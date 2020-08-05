d6c5d9d343ca855ec6c385568c7064c658633a0a
Emily Janzer <emily.janzer@gmail.com>
2020-08-05 13:29:22 -0700

    Add iframe from Voter's Edge

--- src/pages/findYourBallot.js
+++ src/pages/findYourBallot.js
@@ -1,3 +1,4 @@
+import IframeResizer from "iframe-resizer-react"
 import LandingPageHero from "../components/landingPageHero"
 import Layout from "../components/layout"
 import React from "react"
@@ -16,7 +17,14 @@ export default function FindYourBallot() {
         <h2>Get your personalized ballot</h2>
         <p>Get information on candidates, measures and who supports them</p>
         <div className={styles.iframe}>
-          <p>TODO</p>
+          <IframeResizer
+            log
+            src="https://votersedge.org/en/ca/address-widget"
+            maxWidth="600px"
+            minWidth="240px"
+            height="239px"
+            frameBorder="0"
+          />
         </div>
       </div>
     </Layout>
