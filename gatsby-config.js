4e20132d84523fe1506c9568c8dbc8b75b60824e
Emily Janzer <emily.janzer@gmail.com>
2020-08-24 20:33:21 -0700

    Add GraphQL query to /candidates page

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,5 +5,28 @@
  */
 
 module.exports = {
-  plugins: [`gatsby-plugin-sass`],
+  plugins: [
+    `gatsby-plugin-sass`,
+    {
+      resolve: "gatsby-source-apiserver",
+      options: {
+        url: `http://localhost:5000/open-disclosure/api/v1.0/candidates`,
+        method: "get",
+        data: {},
+        name: `candidates`,
+        entityLevel: `Candidates`,
+        schemaType: {
+          Name: "String",
+          Elections: [
+            {
+              ElectionCycle: "String",
+              ElectionTitle: "String",
+              Committees: [{ Name: "String", TotalFunding: 1 }],
+            },
+          ],
+        },
+        enableDevRefresh: true,
+      },
+    },
+  ],
 }
--- src/pages/candidates.js
+++ src/pages/candidates.js
@@ -1,5 +1,26 @@
 import React from "react"
 
-export default function Candidates() {
-  return <div />
+export default function Candidates({ data }) {
+  return (
+    <ul>
+      {data.allCandidates.edges
+        .filter(({ node }) => node.id !== "dummy")
+        .map(({ node }) => (
+          <li key={node.id}>{node.Name}</li>
+        ))}
+    </ul>
+  )
 }
+
+export const query = graphql`
+  query {
+    allCandidates {
+      edges {
+        node {
+          id
+          Name
+        }
+      }
+    }
+  }
+`
