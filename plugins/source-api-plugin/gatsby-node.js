5a832c51118af56ae4edaea68c3ba80ae27aecb5
Emily Janzer <emily.janzer@gmail.com>
2020-08-28 21:27:01 -0700

    fetch data from api if available, otherwise fall back to dummy data

--- plugins/source-api-plugin/gatsby-node.js
+++ plugins/source-api-plugin/gatsby-node.js
@@ -1,27 +1,41 @@
+const fetch = require("node-fetch")
+
+// TODO Add dev/prod variations
+const HOSTNAME = process.env.GATSBY_API_HOST || "localhost:5000"
 const CANDIDATE_NODE_TYPE = `candidate`
+
+const DUMMY_DATA = {
+  Candidates: [
+    {
+      Name: "Lindsay Lohan",
+      Elections: [
+        {
+          ElectionCycle: "2020 Election Cycle",
+          ElectionTitle: "District 9 Representative",
+          Committees: [{ Name: "Lindsay Lohan 2020", TotalFunding: 300 }],
+        },
+      ],
+    },
+  ],
+}
 
 exports.sourceNodes = async ({
   actions,
   createNodeId,
   createContentDigest,
-  getNodesByType,
 }) => {
   const { createNode } = actions
 
-  // This can become fallback data?
-  const data = {
-    Candidates: [
-      {
-        Name: "Lindsay Lohan",
-        Elections: [
-          {
-            ElectionCycle: "2020 Election Cycle",
-            ElectionTitle: "District 9 Representative",
-            Committees: [{ Name: "Lindsay Lohan 2020", TotalFunding: 300 }],
-          },
-        ],
-      },
-    ],
+  let data
+  try {
+    const response = await fetch(
+      `http://${HOSTNAME}/open-disclosure/api/v1.0/candidates`
+    )
+    data = await response.json()
+  } catch (networkError) {
+    // Use dummy data as a fallback in case the API isn't available.
+    // TODO This is only for development purposes - get rid of this
+    data = DUMMY_DATA
   }
 
   data.Candidates.forEach(candidate => {
