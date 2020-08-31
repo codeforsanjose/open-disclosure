0f843c40f2250e998eb1913da0663402b917938b
Emily Janzer <emily.janzer@gmail.com>
2020-08-30 20:47:04 -0700

    Add a request to the /elections endpoint to the API plugin

--- plugins/source-api-plugin/gatsby-node.js
+++ plugins/source-api-plugin/gatsby-node.js
@@ -3,35 +3,56 @@ const fetch = require("node-fetch")
 // TODO Add dev/prod variations
 const HOSTNAME = process.env.GATSBY_API_HOST || "localhost:5000"
 const CANDIDATE_NODE_TYPE = `candidate`
+const ELECTION_NODE_TYPE = `election`
 
 const DUMMY_DATA = {
-  Candidates: [
-    {
-      Name: "Lindsay Lohan",
-      Elections: [
-        {
-          ElectionCycle: "2020 Election Cycle",
-          ElectionTitle: "District 9 Representative",
-          Committees: [{ Name: "Lindsay Lohan 2020", TotalFunding: 300 }],
-        },
-      ],
-    },
-  ],
+  candidates: {
+    Candidates: [
+      {
+        Name: "Lindsay Lohan",
+        Elections: [
+          {
+            ElectionCycle: "2020 Election Cycle",
+            ElectionTitle: "District 9 Representative",
+            Committees: [{ Name: "Lindsay Lohan 2020", TotalFunding: 300 }],
+          },
+        ],
+      },
+    ],
+  },
+  elections: {
+    ElectionCycles: [
+      {
+        Title: "2020 Election Cycle",
+        Date: "2020-11-15",
+        TotalContributions: 1000,
+        OfficeElections: [
+          {
+            Title: "District 9 Representative",
+            Candidates: ["Jake John", "Lindsay Lohan"],
+            TotalContributions: 300,
+          },
+        ],
+        Referendums: [
+          {
+            Title: "Ballot Measure C",
+            Description: "This ballot measure will allow people to have fun",
+            "Total Contributions": 700,
+          },
+        ],
+      },
+    ],
+  },
 }
 
-exports.sourceNodes = async ({
-  actions,
-  createNodeId,
-  createContentDigest,
-}) => {
-  const { createNode } = actions
-
-  let data
+async function fetchEndpoint(endpoint) {
   try {
     const response = await fetch(
-      `http://${HOSTNAME}/open-disclosure/api/v1.0/candidates`
+      `http://${HOSTNAME}/open-disclosure/api/v1.0/${endpoint}`
     )
-    data = await response.json()
+    if (response.ok) {
+      return await response.json()
+    }
   } catch (networkError) {
     console.warn(
       "Unable to reach the API server at " +
@@ -41,12 +62,22 @@ exports.sourceNodes = async ({
     console.info(
       "Use the environment variable GATSBY_API_HOST to use a different hostname for the API server"
     )
-    // Use dummy data as a fallback in case the API isn't available.
-    // TODO This is only for development purposes - get rid of this
-    data = DUMMY_DATA
   }
+  return DUMMY_DATA[endpoint]
+}
 
-  data.Candidates.forEach(candidate => {
+exports.sourceNodes = async ({
+  actions,
+  createNodeId,
+  createContentDigest,
+}) => {
+  const { createNode } = actions
+
+  const [candidateData, electionData] = await Promise.all([
+    fetchEndpoint("candidates"),
+    fetchEndpoint("elections"),
+  ])
+  candidateData.Candidates.forEach(candidate => {
     createNode({
       ...candidate,
       id: createNodeId(`${CANDIDATE_NODE_TYPE}-${candidate.id}`),
@@ -56,6 +87,19 @@ exports.sourceNodes = async ({
         type: CANDIDATE_NODE_TYPE,
         content: JSON.stringify(candidate),
         contentDigest: createContentDigest(candidate),
+      },
+    })
+  })
+  electionData.ElectionCycles.forEach(election => {
+    createNode({
+      ...election,
+      id: createNodeId(`${ELECTION_NODE_TYPE}-${election.id}`),
+      parent: null,
+      children: [],
+      internal: {
+        type: ELECTION_NODE_TYPE,
+        content: JSON.stringify(election),
+        contentDigest: createContentDigest(election),
       },
     })
   })
