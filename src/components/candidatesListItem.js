903232306cd2b5fb1b48a3bab7599d77cc4fa346
alessandro-pianetta <alessandro.pianetta@gmail.com>
2020-09-01 14:10:41 -0700

    Creates Candidate Info cmpnt and Candidate List pg

---
+++ src/components/candidatesListItem.js
@@ -0,0 +1,33 @@
+import React from "react"
+import Bar from "./bar"
+import styles from "./candidatesListItem.module.scss"
+
+const percentFormatter = Intl.NumberFormat("en-US", { style: "percent" })
+
+function formatPercent(value) {
+  return percentFormatter.format(value)
+}
+
+export default ({ Name }) => {
+  const percent = formatPercent(123455 / 123456)
+  return (
+    <div className={styles.container}>
+      <img src="https://picsum.photos/125" />
+      <div className={styles.candidate}>
+        <div className={styles.textInfo}>
+          <div className={styles.candidateInfo}>
+            <h1 className={styles.headerText}>{Name}</h1>
+            <p className={styles.label}>CurrentJob</p>
+          </div>
+          <div className={styles.totalRaised}>
+            <p className={styles.label}>Total raised</p>
+            <h2 className={styles.headerText}>$123,456</h2>
+          </div>
+        </div>
+        <div className={styles.visualization}>
+          <Bar type="contributions" percent={percent} />
+        </div>
+      </div>
+    </div>
+  )
+}
--- src/pages/candidates.js
+++ src/pages/candidates.js
@@ -1,21 +1,58 @@
 import { Link, graphql } from "gatsby"
+import Layout from "../components/layout"
+import CandidatesListItem from "../components/candidatesListItem"
 
 import React from "react"
 
 export default function Candidates({ data }) {
+  const election = data.allElection.edges[0].node
+  const candidateInfo = data.allCandidate.edges.map(edge => edge.node)
   return (
-    <ul>
-      {data.allCandidate.edges.map(({ node }) => (
-        <li key={node.id}>
-          <Link to={"/candidate/" + node.fields.slug}>{node.Name}</Link>
-        </li>
-      ))}
-    </ul>
+    <Layout>
+      <ul>
+        {/* We will have to know which office is currently being run for */}
+        {election.OfficeElections[0].Candidates.map(name => {
+          const candidate = candidateInfo.find(node => node.Name === name)
+          const slug = name
+            .split(" ")
+            .join("-")
+            .toLowerCase()
+
+          return (
+            <li key={slug}>
+              {/* Should link to candidate/${node.Date}/${candidateName}} */}
+              <Link to={"/candidate/" + slug}>
+                <CandidatesListItem {...candidate} />
+              </Link>
+            </li>
+          )
+        })}
+      </ul>
+    </Layout>
   )
 }
 
 export const query = graphql`
   query {
+    allElection {
+      edges {
+        node {
+          id
+          Title
+          Date
+          TotalContributions
+          OfficeElections {
+            Title
+            Candidates
+            TotalContributions
+          }
+          Referendums {
+            Title
+            Description
+          }
+        }
+      }
+    }
     allCandidate {
       edges {
         node {
