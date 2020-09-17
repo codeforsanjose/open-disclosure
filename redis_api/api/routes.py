3bb17ffbdc7e9bace8e0b1caa385e182072230ec
geleazar1000111 <geraldine@MacBook-Pro.local>
2020-09-17 10:50:04 -0700

    update fake data endpoints and include fake data bool

--- redis_api/api/routes.py
+++ redis_api/api/routes.py
@@ -16,24 +16,25 @@ def home():
 
 
 @redis_bp.route("/total-contributions", methods=["GET"])
-def get_total_contributions():
+def get_total_contributions(serve_fake=True):
     """
     Query redis to get the total amount of monies spent
     :return: int representing the total for (either all time or current election)
     """
-    try:
-        # TODO re-add real data later
-        # response = r.execute_command("JSON.GET", "TotalContributions")
-        # if not response:
-        #     return empty_response("TotalContributions")
+    if serve_fake:
         return jsonify({"TotalContributions": 100000})
+    try:
+        response = r.execute_command("JSON.GET", "TotalContributions")
+        if not response:
+            return empty_response("TotalContributions")
+        return jsonify({"TotalContributions": json.loads(response)})
     except Exception as error:
         return error_response(f"{error}")
 
 
 @redis_bp.route("/candidates/", defaults={"candidate_id": None}, methods=["GET"])
 @redis_bp.route("/candidates/<string:candidate_id>", methods=["GET"])
-def get_candidates(candidate_id):
+def get_candidates(candidate_id, serve_fake=False):
     """
     Get all the candidates from the current election period or
     a specific candidate 
@@ -42,6 +43,8 @@ def get_candidates(candidate_id):
     :return: data on all candidates or for one specific candidate
     :rtype: JSON
     """
+    if serve_fake:
+        return fake_data.get_candidates_shape()
     try:
         response = r.execute_command("JSON.GET", "Candidates")
         if not response:
@@ -59,50 +62,56 @@ def get_candidates(candidate_id):
 
 
 @redis_bp.route("/committees/", methods=["GET"])
-def get_committees():
+def get_committees(serve_fake=True):
     """
     Get all the committees from the current election period
     :return: list or set of JSON objects containing individual candidate information
     """
-    try:
-        # TODO re-add real data later
-        # response = r.execute_command("JSON.GET", "Committees")
-        # if not response:
-        #     return empty_response("Committees")
+    if serve_fake:
         return jsonify({"Committees": fake_data.get_committees_shape()})
+    try:
+        response = r.execute_command("JSON.GET", "Committees")
+        if not response:
+            return empty_response("Committees")
+        return jsonify({"Committees": json.loads(response)})
     except Exception as error:
         return error_response(f"{error}")
 
 
 @redis_bp.route("/elections/", methods=["GET"])
-def get_elections():
+def get_elections(serve_fake=True):
     """
     Get all the election cycles from 2019-2020
     :return: list or set of JSON objects containing individual election cycle information
     """
-    try:
-        # TODO re-add real data later
-        # response = r.execute_command("JSON.GET", "Elections")
-        # if not response:
-        #     return empty_response("Elections")
+    if serve_fake:
         return jsonify({"Elections": fake_data.get_elections_shape()})
+    try:
+        response = r.execute_command("JSON.GET", "Elections")
+        if not response:
+            return empty_response("Elections")
+        return jsonify({"Elections": json.loads(response)})
     except Exception as error:
         return error_response(f"{error}")
 
 
 @redis_bp.route("/referendums", methods=["GET"])
-def get_referendums():
+def get_referendums(serve_fake=True):
     """
     WIP
     :return:
     """
+    if serve_fake:
+        return fake_data.get_referendums_shape()
     return empty_response("Referendums")
 
 
 @redis_bp.route("/metadata", methods=["GET"])
-def get_metadata():
+def get_metadata(serve_fake=True):
     """
     WIP
     :return:
     """
-    return fake_data.get_metadata_shape()
+    if serve_fake:
+        return fake_data.get_metadata_shape()
+    return empty_response("Metadata")
