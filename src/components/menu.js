3b9f6ce4f6482e3e98fab91bca866bfbaca0c257
alessandro-pianetta <alessandro.pianetta@gmail.com>
2020-09-20 18:06:37 -0700

    Refactors Menu

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,15 +1,27 @@
 import React from "react"
+import NavbarItem from "./navbarItem"
 
 import styles from "./menu.module.scss"
 
-const Menu = ({ children, menuIsOpen, submenu, windowIsLarge }) => (
+const Menu = ({ links, menuIsOpen, submenu, windowIsLarge }) => (
   <ul
-    className={`${styles.navigation} ${styles.closed} ${menuIsOpen && styles.open} ${submenu &&
-      styles.submenu} ${menuIsOpen && submenu && styles.openSubmenu}`
-    }
+    className={`${styles.navigation} ${styles.closed} ${menuIsOpen &&
+      styles.open} ${submenu && styles.submenu} ${menuIsOpen &&
+      submenu &&
+      styles.openSubmenu}`}
   >
-    {(menuIsOpen || windowIsLarge) && children}
+    {(menuIsOpen || windowIsLarge) &&
+      links.map((link, index) => {
+        return (
+          <NavbarItem
+            key={index}
+            menuIsOpen={menuIsOpen}
+            windowIsLarge={windowIsLarge}
+            {...link}
+          />
+        )
+      })}
   </ul>
-);
+)
 
-export default Menu;
+export default Menu
