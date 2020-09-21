cc03d9811b008260f921d351b9b11ebd10b94bfb
alessandro-pianetta <alessandro.pianetta@gmail.com>
2020-09-20 19:44:45 -0700

    Adjusts findYourBallot padding

--- src/pages/findYourBallot.js
+++ src/pages/findYourBallot.js
@@ -15,18 +15,20 @@ export default function FindYourBallot() {
           subtitle="Get the facts before you vote"
         />
       </header>
-      <div className={styles.container}>
-        <h1>Get your personalized ballot</h1>
-        <p>Get information on candidates, measures and who supports them</p>
-        <div className={styles.iframe}>
-          <IframeResizer
-            log
-            src="https://votersedge.org/en/ca/address-widget"
-            minWidth="240px"
-            height="239px"
-            width="100%"
-            frameBorder="0"
-          />
+      <div className={styles.outerContainer}>
+        <div className={styles.innerContainer}>
+          <h1>Get your personalized ballot</h1>
+          <p>Get information on candidates, measures and who supports them</p>
+          <div className={styles.iframe}>
+            <IframeResizer
+              log
+              src="https://votersedge.org/en/ca/address-widget"
+              minWidth="240px"
+              height="239px"
+              width="100%"
+              frameBorder="0"
+            />
+          </div>
         </div>
       </div>
     </Layout>
