a985812ffdce28dfb2e1d21a68cc94f0f8060dac
alessandro-pianetta <alessandro.pianetta@gmail.com>
2020-09-29 19:57:39 -0700

    Improves responsivity of candidate info cards

--- src/components/candidateItem.js
+++ src/components/candidateItem.js
@@ -4,17 +4,17 @@ import { formatDollars } from "../common/util/formatters"
 import styles from "./candidateItem.module.scss"
 
 const CandidateItem = item => (
-  <div className={styles.container} key={item.name}>
-    <img alt="candidate" height="180px" width="180px" src={item.image} />
-    <Link to={item.href}>
-      <div className={styles.candidateInfo}>
-        <h4>{item.name}</h4>
-        <p>{item.position}</p>
-        <h3>{formatDollars(item.amount)}</h3>
-        <p className={styles.amountRaised}>Amount raised</p>
-      </div>
-    </Link>
-  </div>
+  <Link className={styles.container} key={item.name} to={item.href}>
+    <div className={styles.imageContainer}>
+      <img alt="candidate" height="180px" width="180px" src={item.image} />
+    </div>
+    <div className={styles.candidateInfo}>
+      <h4>{item.name}</h4>
+      <p>{item.position}</p>
+      <h3>{formatDollars(item.amount)}</h3>
+      <p className={styles.amountRaised}>Amount raised</p>
+    </div>
+  </Link>
 )
 
 export default CandidateItem
