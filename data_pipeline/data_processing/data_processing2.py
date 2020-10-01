dd0933c758c5b40bc53a8ee6a4c4d85ee1224a16
Darren Pham <phamdarren1@gmail.com>
2020-09-30 20:59:22 -0700

    data queries and data zzz

--- data_pipeline/data_processing/data_processing2.py
+++ data_pipeline/data_processing/data_processing2.py
@@ -23,24 +23,36 @@ import time
 import seaborn as sns
 
 ods = pd.read_csv('460_460A.csv',
-                    # index_col=0,
-                   dtype={'Agent_Nam F': str,
-                          'Sup_Opp_Cd': str,
-                          'G_From_E_F': str,
-                          'XRef_Sch Nm': str,
-                          'Amount': float},
-                   parse_dates=[1,6,7,8,9])
+                     dtype={
+                     # 'Filer_ID': float,
+                     'Agent_Nam F': str,
+                     'Sup_Opp_Cd': str,
+                     'G_From_E_F': str,
+                     'XRef_Sch Nm': str,
+                     'Amount': float
+                     },
+                     parse_dates=[0,7,8,9,10])
 
 ods.shape
 
 # In[2]:
 
+# Trying to fix Filer_ID as string
+# for row in ods.iterrows():
+#    if type(row[1][5]) == str:
+#        row[1][5] = 0.0
+#    print(row[1][5])
+
+# ods['Filer_ID'] = ods['Filer_ID'].astype(float)
+
+# In[2]:
+
 # Filter out rows based on if they're new
-twentytwenty_nov = '11/1/2020'
+# twentytwenty_nov = '11/1/2020'
 
-mask = ods['Election Date'] >= twentytwenty_nov
+# mask = ods['Election_Date'] >= twentytwenty_nov
 
-ods = ods.loc[mask]
+# ods = ods.loc[mask]
 
 # In[3]:
 
@@ -71,22 +83,25 @@ new_ods.columns = [c.strip().replace(' ', '_') for c in ods.columns]
 
 ### Save the new dataset back to disk
 # Pandas can save the new processed dataset in csv, json or sql format.
-# We will save it to csv in this case.
+# # We will save it to csv in this case.
 new_ods.to_csv("460_460A_reduced.csv")
 
 
 
-# In[7]:
+# # In[7]:
 
-# ### Save to MySQL database
-# 1. Setup MySQL connector
-# 2. Connect MySQL database
-# 3. Save dataset to MySQL
-#     Note: it will purge existing table and replace with the new data.
+# # ### Save to MySQL database
+# # 1. Setup MySQL connector
+# # 2. Connect MySQL database
+# # 3. Save dataset to MySQL
+# #     Note: it will purge existing table and replace with the new data.
 import pymysql
 from sqlalchemy import create_engine
 
 pymysql.install_as_MySQLdb()
-conn = create_engine("mysql+mysqldb://admin:@database-1.c2fzqal0xakt.us-west-1.rds.amazonaws.com/test2")
+conn = create_engine("mysql+mysqldb://admin:QEruiTUa7GjHVrQUnP2P@database-1.c2fzqal0xakt.us-west-1.rds.amazonaws.com/test2")
 new_ods.to_sql(con=conn, name='test2', if_exists='replace', index=True)
-# %%
+
+with conn.connect() as con:
+       con.execute('ALTER TABLE `test2` ADD PRIMARY KEY (`Index`);')
+
--- data_pipeline/data_processing/data_queries.py
+++ data_pipeline/data_processing/data_queries.py
@@ -1,7 +1,12 @@
+import datetime
 import pymysql
 pymysql.install_as_MySQLdb()
 
 from sqlalchemy import create_engine
+# from sqlalchemy.ext.declarative import declarative_base
+# from sqlalchemy.orm import sessionmaker
+# from sqlalchemy import func
+# from sqlalchemy import distinct
 
 import pprint
 pp = pprint.PrettyPrinter(indent=4)
@@ -9,10 +14,35 @@ pp = pprint.PrettyPrinter(indent=4)
 class Data_Query:
 
   def __init__(self):
-    self.ELECTION_DATE = '2020-11-3'
-    self.engine = create_engine('mysql+mysqldb://admin:QEruiTUa7GjHVrQUnP2P@database-1.c2fzqal0xakt.us-west-1.rds.amazonaws.com/test2')
+    self.ELECTION_DATE = ''
+    self.engine = create_engine('mysql+mysqldb://app_user:Dn4wPfGnT78d4FtT@database-1.c2fzqal0xakt.us-west-1.rds.amazonaws.com/test2')
+    # Base = declarative_base(self.engine)
+
+    # class Test2(Base):
+    #   """
+    #   eg. fields: id, title
+    #   """
+    #   __tablename__ = 'test2'
+    #   __table_args__ = {'autoload': True}
+
+    # metadata = Base.metadata
+    # self.session = sessionmaker(bind=self.engine)()
+
+    # try:
+    #   # res = self.session.query(func.count(Test2.Election_Date))
+    #   # print(res)
+    #   res = self.session.query(distinct(Test2.Election_Date))
+    #   print(res)
+    #   for r in res:
+    #     print(r)
+    # except:
+    #   self.session.rollback()
+    #   raise
+    # finally:
+    #   self.session.close()
+
     self.candidates_data = {"Candidates": []}
-    self.election_data = {"Elections": []}
+    self.election_data = {"Elections": {}}
 
   def execute_query(self, query):
     output = []
@@ -20,16 +50,24 @@ class Data_Query:
       rs = conn.execute(query)
       for row in rs:
         output.append(row)
+
     return output
 
   def lc_n_rm_spc(self, s):
     return s.strip().lower().replace(' ', '-')
 
+  
+  def extract_latest_election(self):
+    self.ELECTION_DATE = self.execute_query('SELECT DISTINCT max(date(Election_Date)) FROM test2;')[0][0]
+    # self.ELECTION_DATE = "2020-3-3"
+    self.ELECTION_DATE_US = datetime.datetime.strptime(self.ELECTION_DATE, '%Y-%m-%d').strftime('%m/%d/%y')
+    # print(self.ELECTION_DATE)
+
 
   def retrieve_candidates(self):
     self.candidates = []
 
-    q = self.execute_query('SELECT DISTINCT CandidateControlledName FROM test2;')
+    q = self.execute_query('SELECT DISTINCT CandidateControlledName FROM test2 WHERE Election_Date = "{}";'.format(self.ELECTION_DATE))
     for candidate in q:
       self.candidates.append(candidate[0])
     self.candidates.remove('Independent')
@@ -44,57 +82,116 @@ class Data_Query:
       committee = candidate_info[0][0]
       district = candidate_info[0][1]
       election_date = candidate_info[0][2]
-      total_funding = self.execute_query('SELECT SUM(Amount) FROM test2 WHERE Filer_Nam_L = "{}" AND Rec_Type = "RCPT";'.format(committee))[0][0]
+      committee_total_funding = self.execute_query('SELECT SUM(Amount) FROM test2 WHERE Filer_Nam_L = "{}" AND (Rec_Type = "S497" OR Rec_Type = "RCPT") AND Election_Date = "{}";'.format(committee, self.ELECTION_DATE))[0][0]
+      candidate_total_funding = self.execute_query('SELECT SUM(Amount) FROM test2 WHERE CandidateControlledName = "{}" AND (Rec_Type = "S497" OR Rec_Type = "RCPT") AND Election_Date = "{}";'.format(candidate.replace('"','\\"'), self.ELECTION_DATE))[0][0]
+      
+
+      contributors_data = []
+      contributors = self.execute_query('SELECT Entity_Nam_F, Entity_Nam_L, Entity_Cd, Entity_Occ, Entity_ZIP4, Amount, Tran_Date  FROM test2 WHERE CandidateControlledName = "{}" AND (Rec_Type = "S497" OR Rec_Type = "RCPT") AND Election_Date = "{}";'.format(candidate.replace('"','\\"'),self.ELECTION_DATE))
+      for c in contributors:
+        name = ''
+        if c[0] is not None and c[1] is not None:
+          name = "{} {}".format(c[0],c[1]) 
+        elif c[0] is None and c[1] is not None:
+          name = c[1]
+        else:
+          name = "Anonymous Donor"
+        
+        contributors_data.append({
+          "Name": name,
+          "Contributor_Type": c[2],
+          "Occupation": c[3],
+          "Zip_Code": c[4][:5],
+          "Amount": c[5],
+          "Date": datetime.date.strftime(datetime.datetime(1899, 12, 30) + datetime.timedelta(days=c[6]), "%m/%d/%Y")
+        })
+      
+      
       self.candidates_data['Candidates'].append({
-        "ID": "{};{};{}".format(self.lc_n_rm_spc(district), candidate, election_date),
+        "ID": "{};{};{}".format(self.lc_n_rm_spc(district), self.lc_n_rm_spc(candidate), election_date),
+        "Name": candidate,
+        "TotalFunding": round(candidate_total_funding, 2),
         "Committees": [
           {
             "ID": "{};{}".format(self.lc_n_rm_spc(committee), election_date),
             "Name": "{}".format(committee),
-            "Total Funding": round(total_funding, 2)
+            "TotalFunding": round(committee_total_funding, 2)
           }
-        ]
+        ],
+        "Contributors": contributors_data 
       })
-    pp.pprint(self.candidates_data)
-    
-  def extract_election(self):
-    self.elections = []
+    # pp.pprint(self.candidates_data)
 
-    q = self.execute_query('SELECT DISTINCT date(Election_Date) FROM test2;')
-    for candidate in q:
-      self.elections.append(candidate[0])
-    # print(self.elections[0])
   
   def extract_data_for_election(self):
-    office_elections_data = {}
-    total_contributions = self.execute_query('SELECT SUM(Amount) FROM test2 WHERE CandidateControlledName != "Independent" AND Rec_Type = "RCPT" AND Election_Date = "{}";'.format(self.ELECTION_DATE))[0][0]
-    office_elections = self.execute_query('SELECT DISTINCT Ballot_Item FROM test2 WHERE CandidateControlledName != "Independent" AND Election_Date = "{}"'.format(self.ELECTION_DATE))
+    total_contributions = self.execute_query('SELECT SUM(Amount) FROM test2 WHERE CandidateControlledName != "Independent" AND (Rec_Type = "S497" OR Rec_Type = "RCPT") AND Election_Date = "{}";'.format(self.ELECTION_DATE))[0][0]
     
+    ### Retrieve Office Data
+    # Retrieve All Elections for Office in N election cycle
+    office_elections = self.execute_query('SELECT DISTINCT Ballot_Item FROM test2 WHERE CandidateControlledName != "Independent" AND Election_Date = "{}"'.format(self.ELECTION_DATE))
+
+    # Loop through elections
+    office_elections_data = []
     for office in office_elections:
+      # Retrieve Candidates for the Election/Office
       candidates = self.execute_query('SELECT DISTINCT CandidateControlledName FROM test2 WHERE Ballot_Item = "{}" AND Election_Date = "{}"'.format(office[0], self.ELECTION_DATE))
-      
-      office_elections_data[office[0]] = []
-      for c in candidates:
-        if c[0] != "Independent":
-          office_elections_data[office[0]].append(c[0])
 
-      office_contributions = self.execute_query('SELECT SUM(Amount) FROM test2 WHERE Ballot_Item = "{}" AND Election_Date = "{}"'.format(office[0], self.ELECTION_DATE))[0]
-      office_elections_data[office[0]].append(round(office_contributions[0],2))
+      # Retrieve total contributions per Office Election
+      office_contributions = self.execute_query('SELECT SUM(Amount) FROM test2 WHERE Ballot_Item = "{}" AND Election_Date = "{}" AND (Rec_Type = "S497" OR Rec_Type = "RCPT")'.format(office[0], self.ELECTION_DATE))[0]
 
-    print(office_elections_data)
-    self.election_data["Elections"].append({
-      self.ELECTION_DATE: {"Total Contributions": round(total_contributions, 2),
-      "Office Elections": []}
-    })
-
-    for office in office_elections_data:
-      print(office)
+      office_elections_data.append({
+        "Title": office[0].replace('-', ' '),
+        "CandidateIDs": ["{};{}".format(self.lc_n_rm_spc(c[0]),self.ELECTION_DATE_US) for c in candidates if c[0] != "Independent"],
+        "TotalContributions": round(office_contributions[0],2)
+      })
 
 
-    print(self.election_data)
+    ### Retrieve Referendum/Ballot Measure Data
+    referendum_data = []
+    referendums = self.execute_query('SELECT DISTINCT Ballot_Item FROM test2 WHERE Ballot_Type = "Ballot Measure" AND Election_Date = "{}";'.format(self.ELECTION_DATE))
+    for r in referendums:
+      print(r)
+      referendum_letter = ""
+      headers = ["Bal_Num", "G_From_E_F"]
+      for h in headers:
+        try:
+          measure_letter = self.execute_query('SELECT DISTINCT {} from test2 where Ballot_Item = "{}";'.format(h, r[0]))
+          measure_contributions = self.execute_query('SELECT SUM(Amount) FROM test2 WHERE Ballot_Item = "{}" AND (Rec_Type = "S497" OR Rec_Type = "RCPT");'.format(r[0]))
+          for letter in measure_letter:
+            for i in range(2):
+              for j in range(2):
+                try:
+                  if letter[i][j] is not None:
+                    referendum_letter = letter[i][j]
+                except:
+                  pass
+        except:
+          pass
+      referendum_data.append({
+        "Title": "Ballot Measure {}".format(referendum_letter),
+        "Description": "TBD",
+        "TotalContributions": round(measure_contributions[0][0], 2)
+      })
+
+    ### Export data into Class variable
+    self.election_data["Elections"] = {
+      self.ELECTION_DATE_US: {
+      "TotalContributions": round(total_contributions, 2),
+      "Date": self.ELECTION_DATE,
+      "Office Elections": office_elections_data,
+      "Referendums": referendum_data
+      }
+    }
+
+  
+  def metadata(self):
+    self.election_data["metadata"] = {"DateProcesses": datetime.date.strftime(datetime.date.today(), "%Y/%m/%d")}
+
+    # pp.pprint(self.election_data)
 
 data = Data_Query()
+data.extract_latest_election()
 data.retrieve_candidates()
 data.extract_data_per_candidate()
-data.extract_election()
 data.extract_data_for_election()
+data.metadata()
