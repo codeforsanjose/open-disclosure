f25e008bebdfa10359394c2aca3feec03454949c
alessandro-pianetta <alessandro.pianetta@gmail.com>
2020-10-27 17:21:28 -0700

    Adds team info

---
+++ src/components/teamMembers.js
@@ -0,0 +1,32 @@
+import React from "react"
+import styles from "./teamMembers.module.scss"
+import Blank from "../images/blankProfile.png"
+
+const PersonalInfo = ({ position, name, image }) => (
+  <>
+    <img src={image || Blank} alt={`Headshot of ${name}`} />
+    <h1>{name}</h1>
+    <p>{position}</p>
+  </>
+)
+
+export default function TeamMembers({ team }) {
+  return (
+    <ul className={styles.container}>
+      {team.map(teammate => (
+        <li
+          className={styles.individual}
+          key={`${teammate.position}-${teammate.name}`}
+        >
+          {teammate.github ? (
+            <a href={`https://github.com/${teammate.github}`}>
+              <PersonalInfo {...teammate} />
+            </a>
+          ) : (
+            <PersonalInfo {...teammate} />
+          )}
+        </li>
+      ))}
+    </ul>
+  )
+}
