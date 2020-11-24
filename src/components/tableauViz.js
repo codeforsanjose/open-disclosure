8490b90b457d4b2f4c9d3d6daa190305be894f4b
alessandro-pianetta <alessandro.pianetta@gmail.com>
2020-11-23 19:24:47 -0800

    Updated tableau react and made cmpnt class-based

--- src/components/tableauViz.js
+++ src/components/tableauViz.js
@@ -2,24 +2,42 @@ import React from "react"
 import styles from "./tableauViz.module.scss"
 import TableauReport from "tableau-react"
 
-const TableauViz = ({ candidateName }) => {
-  // const [firstName, lastName] = candidateName.split(" ")
-  console.log(candidateName)
-  return (
-    <div className={styles.container}>
-      <TableauReport
-        url="https://public.tableau.com/views/SanJosecampaigndata/Dashboardmap"
-        options={{
-          height: 600,
-          width: 1000,
-          hideTabs: false,
-          // All other vizCreate options are supported here, too
-          // They are listed here: https://help.tableau.com/current/api/js_api/en-us/JavaScriptAPI/js_api_ref.htm#vizcreateoptions_record
-        }}
-        // query={`language=en&Candidates=${firstName}%20${lastName}`}
-      />
-    </div>
-  )
+class TableauViz extends React.Component {
+  constructor(props) {
+    super(props)
+    this.viz = null
+    this.setVizRef = ref => {
+      this.viz = ref
+    }
+  }
+
+  componentDidMount() {
+    console.log(this.viz.getUrl())
+  }
+
+  render() {
+    const { candidateName } = this.props
+    const [firstName, lastName] = candidateName.split(" ")
+
+    return (
+      <div className={styles.container}>
+        <TableauReport
+          url="https://public.tableau.com/views/SanJosecampaigndata/Dashboardmap"
+          ref={this.setVizRef}
+          options={{
+            height: 727,
+            width: "100%",
+            hideTabs: false,
+            "San Jose Districts": 4,
+            Candidates: candidateName,
+            // All other vizCreate options are supported here, too
+            // They are listed here: https://help.tableau.com/current/api/js_api/en-us/JavaScriptAPI/js_api_ref.htm#vizcreateoptions_record
+          }}
+          query={`?:Candidates=${firstName}%20${lastName}`}
+        />
+      </div>
+    )
+  }
 }
 
 export default TableauViz
