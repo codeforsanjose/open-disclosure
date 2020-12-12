577977c57e4952da1fdb8c299c1e87611693f3f9
alessandro-pianetta <alessandro.pianetta@gmail.com>
2020-12-11 17:08:47 -0800

    Implements loadable

--- src/templates/candidate.js
+++ src/templates/candidate.js
@@ -3,12 +3,13 @@ import {
   // Link,
   graphql,
 } from "gatsby"
+import loadable from "@loadable/component"
 // Components
 import Layout from "../components/layout"
 import SideNav from "../components/sideNav"
 import SectionHeader from "../components/sectionHeader"
 import ChartSection from "../components/chartSection"
-import TableauViz from "../components/tableauViz"
+const TableauViz = loadable(() => import("../components/tableauViz"))
 import { TotalAmountPanelItem } from "../components/totalAmountItem"
 import NoData from "../components/noData"
 // Styles
